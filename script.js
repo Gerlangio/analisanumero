5d3f06176ada52955265d5d3c712c8d4b6a9f90d
gerlangiosoares <gerlangio@gmail.com>
2022-06-24 12:55:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Essas são variáveis GLOBAIS
 let num = window.document.querySelector('input#fnum');
 let lista = window.document.querySelector('select#flista');
 let res = window.document.querySelector('div#res');
