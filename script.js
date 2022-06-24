b5577185dac333da41b17a5b300157486feb94bf
gerlangiosoares <gerlangio@gmail.com>
2022-06-24 13:01:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 let num = window.document.querySelector('input#fnum');
 let lista = window.document.querySelector('select#flista');
 let res = window.document.querySelector('div#res');
+let valores = [];
 
 function adicionar() {
+    //Para adicionar verifica se é número e se não já está na lista
+    if (isNumero(num.value) && !inLista(num.value)) {
 
+    } else {
+        window.alert('Valor inválido ou encontrado na lista');
+    }
 }
