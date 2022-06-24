04d2c4d49642ca8626899ae0650d490c7e276d25
gerlangiosoares <gerlangio@gmail.com>
2022-06-24 13:25:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,19 @@
 let num = window.document.querySelector('input#fnum');
 let lista = window.document.querySelector('select#flista');
 let res = window.document.querySelector('div#res');
-let valores = [];
+let valores = []; //O vetor é uma lista
+
+function isNumero(n) {
+
+}
+
+function inLista(n, l) {
+
+}
 
 function adicionar() {
-    //Para adicionar verifica se é número e se não já está na lista
-    if (isNumero(num.value) && !inLista(num.value)) {
+    //Para adicionar verifica se é número e verifica se já está na lista
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
 
     } else {
         window.alert('Valor inválido ou encontrado na lista');
