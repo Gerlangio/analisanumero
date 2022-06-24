ceab789ca511336244dfd873313afd7cef555dc6
gerlangiosoares <gerlangio@gmail.com>
2022-06-24 12:54:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let num = window.document.querySelector('input#fnum');
 let lista = window.document.querySelector('select#flista');
+let res = window.document.querySelector('div#res');
 
 function adicionar() {
 
