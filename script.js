84574f34a83019b31b69464067707a3672897a3f
gerlangiosoares <gerlangio@gmail.com>
2022-06-24 14:31:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,9 @@ function adicionar() {
     num.value = '';
     num.focus();
 }
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    }
+}
