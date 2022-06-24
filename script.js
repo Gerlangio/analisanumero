edc4320e0630422b3cd521c08e141a2dd5bc78b3
gerlangiosoares <gerlangio@gmail.com>
2022-06-24 13:41:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function inLista(n, l) {
 function adicionar() {
     //Para adicionar verifica se é número e verifica se já está na lista
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-
+        window.alert('Tudo Ok!');
     } else {
         window.alert('Valor inválido ou encontrado na lista');
     }
