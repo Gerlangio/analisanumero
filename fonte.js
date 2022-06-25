0e71d09877caae978898e3f67075acab5d9e50f3
gerlangiosoares <gerlangio@gmail.com>
2022-06-25 11:37:00 -0300

    Create fonte.js

---
+++ fonte.js
@@ -0,0 +1,12 @@
+/*
+let amigo = [];
+console.log(typeof amigo);//mostra: object
+//
+let amigo = {nome: 'José', sexo: 'M', peso: 85.4};
+console.log(amigo);
+*/
+let amigo = {
+    nome: 'José', 
+    sexo: 'M', 
+    peso: 85.4};
+console.log(amigo);
