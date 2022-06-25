aa3863f81adcd5758e8d648d5005d310e066ff5c
gerlangiosoares <gerlangio@gmail.com>
2022-06-25 17:17:25 -0300

    Update fonte.js

--- fonte.js
+++ fonte.js
@@ -8,5 +8,12 @@ console.log(amigo);
 let amigo = {
     nome: 'José', 
     sexo: 'M', 
-    peso: 85.4};
-console.log(amigo);
+    peso: 85.4, 
+    engordar(p=0){
+        console.log('Engordou!')
+        this.peso += p;
+    }
+};
+amigo.engordar(2);
+//console.log(amigo.nome);
+console.log(`${amigo.nome} pesa ${amigo.peso} kg.`)
